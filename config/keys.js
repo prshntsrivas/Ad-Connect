bfbfb18e5583c0c39e88d8bca2481379c56a8264
unknown <prashantsrivastava0197@gmail.com>
2021-07-19 15:29:02 +0530

    Client Added

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGOURL: "mongodb://localhost:27017/User",
+  JWT_SECRET: "Prashant",
+  razorIdkey: "Prashant",
+  razorIdSecret: "Prashant",
+  emailId: "prashantsrivastava0197@gmail.com",
+  emailPassword: "1234",
+};
